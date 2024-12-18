a3e8611edf0cf7ac1c0f24f9d78835e3f2b04856
Arthuryan Loheac <146721013+ArthuryanLoheac@users.noreply.github.com>
2024-12-18 12:31:16 +0100

    deck ui changes

--- BoosterDefense-main/Assets/Scripts/UI/CardsBoosterDeck/Cards/CheckMaxCardHand.cs
+++ BoosterDefense-main/Assets/Scripts/UI/CardsBoosterDeck/Cards/CheckMaxCardHand.cs
@@ -7,11 +7,14 @@ using UnityEngine.UI;
 public class CheckMaxCardHand : MonoBehaviour
 {
     public string textNotActive;
+    public Color colorNotActive;
+    private Color colorActive;
     private string textActive;
 
     void Start()
     {
         textActive = transform.GetChild(0).GetComponent<TMP_Text>().text;
+        colorActive = transform.GetChild(0).GetComponent<TMP_Text>().color;
     }
     // Update is called once per frame
     void Update()
@@ -19,9 +22,11 @@ public class CheckMaxCardHand : MonoBehaviour
         if (DeckCardsManager.instance.StartHand.Count < 5) {
             GetComponent<Button>().interactable = true;
             transform.GetChild(0).GetComponent<TMP_Text>().text = textActive;
+            transform.GetChild(0).GetComponent<TMP_Text>().color = colorActive;
         } else {
             GetComponent<Button>().interactable = false;
             transform.GetChild(0).GetComponent<TMP_Text>().text = textNotActive;
+            transform.GetChild(0).GetComponent<TMP_Text>().color = colorNotActive;
         }
     }
 }
