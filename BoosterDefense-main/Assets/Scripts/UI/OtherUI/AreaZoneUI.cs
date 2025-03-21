61f88742778784d215ecd2f10b7bc0f965b36c25
Arthuryan Loheac <146721013+ArthuryanLoheac@users.noreply.github.com>
2025-03-21 20:53:46 +0100

    :sparkles: new UI circle Area

--- BoosterDefense-main/Assets/Scripts/UI/OtherUI/AreaZoneUI.cs
+++ BoosterDefense-main/Assets/Scripts/UI/OtherUI/AreaZoneUI.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class AreaZoneUI : MonoBehaviour
 {
     public float valueDefault = 0;
+    public float valueWeapon = 0;
+    public float valueRessource = 0;
+    public float valueDetection = 0;
 
     public Weapon weapon;
     public NPCStats npcStats;
@@ -13,11 +16,24 @@ public class AreaZoneUI : MonoBehaviour
     void Start()
     {
         GameObject areaInteract;
-        if (valueDefault != 0)
-        {
+        if (valueDefault != 0) {
             areaInteract = Instantiate(UIManager.instance.getDefaultArea(), transform);
             areaInteract.transform.localScale = areaInteract.transform.localScale * valueDefault;
         }
+        if (valueWeapon != 0) {
+            areaInteract = Instantiate(UIManager.instance.getWeaponArea(), transform);
+            areaInteract.transform.localScale = areaInteract.transform.localScale * valueWeapon;
+        }
+        if (valueRessource != 0) {
+            areaInteract = Instantiate(UIManager.instance.getCollectArea(), transform);
+            areaInteract.transform.localScale = areaInteract.transform.localScale * valueRessource;
+        }
+        if (valueDetection != 0) {
+            areaInteract = Instantiate(UIManager.instance.getDetectionArea(), transform);
+            areaInteract.transform.localScale = areaInteract.transform.localScale * valueDetection;
+        }
+
+
         if (weapon != null)
         {
             areaInteract = Instantiate(UIManager.instance.getWeaponArea(), transform);
