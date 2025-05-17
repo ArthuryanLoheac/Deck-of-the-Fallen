785441841128c4f4b4ade9d082b38f0a155da430
Arthuryan Loheac <146721013+ArthuryanLoheac@users.noreply.github.com>
2025-05-17 16:59:05 +0200

    :sparkles: Show cards

--- BoosterDefense-main/Assets/MulliganManager.cs
+++ BoosterDefense-main/Assets/MulliganManager.cs
@@ -6,6 +6,8 @@ public class MulliganManager : MonoBehaviour
 {
     public bool isInMulligan;
     public static MulliganManager instance;
+    public GameObject mulliganPrefabCard;
+    float widthCard = 0;
 
     public void Awake()
     {
@@ -15,10 +17,18 @@ public class MulliganManager : MonoBehaviour
     public void Start()
     {
         isInMulligan = false;
+        widthCard = mulliganPrefabCard.GetComponent<RectTransform>().sizeDelta.x;
     }
 
     IEnumerator MulliganDraw()
     {
+        int draw = 5;
+        for (int i = 0; i < draw; i++)
+        {
+            Vector3 pos = transform.position;
+            pos.x += (i - (draw / 2)) * (widthCard * 1.5f);
+            Instantiate(mulliganPrefabCard, pos, Quaternion.identity, transform);
+        }
         yield return new WaitForSeconds(5.0f);
         isInMulligan = false;
         Debug.Log("MULLIGAN");
